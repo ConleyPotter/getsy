19c3cd028dcb4fa9a450e836e4c55fa0f1f05ea2
Conley Potter <conley.potter@gmail.com>
2019-06-03 17:37:33 -0500

    Add backend routes for users

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.get("/", (req, res) => {
 
   res.send("Checking Mongoose");
 })
+app.use("/api/users", users);
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
