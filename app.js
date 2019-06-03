629cda723b5fcd4d6e07c5378ace75935af72af2
david <dsuon04@gmail.com>
2019-06-03 15:04:04 -0700

    user model

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ const express = require("express");
 
 const app = express();
 const db = require('./config/keys').mongoURI;
+const User = require('./models/User');
+const bodyParser = require('body-parser');
+
 mongoose 
   .connect(db, { useNewUrlParser: true})
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Checking Mongoose"));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  
+  // const user1 = new User({
+  //   email: 'detective@ace.com',
+  //   fName: 'Jimmy',
+  //   password: 'salt'
+  // });
+  // user1.save();
+
+  res.send("Checking Mongoose");
+})
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
---
+++ routes/api/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../../models/User");
+
+router.get("/test", (req, res) => {
+    res.json({msg: "In the user route"})
+});
+
+router.post('/register', (req,res)=>{
+    User.findOne({ email: req.body.email})
+    .then(user => {
+        if (user) {
+            return res.status(400).json({email: "A user with that email is registered"})
+        } else {
+            const newUser = new User({
+                fName: req.body.fName,
+                email: req.body.email,
+                password: req.body.password
+            })
+            newUser.save().then(user => res.send(user)).catch(err => res.send(err));
+        }
+    })
+})
