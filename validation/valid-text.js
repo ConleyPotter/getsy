6d029f90d605b7cfe03e1350233ebb5b4f9b06fc
david <dsuon04@gmail.com>
2019-06-04 09:38:28 -0700

    readme initial

--- validation/valid-text.js
+++ validation/valid-text.js
@@ -1,4 +1,4 @@
 const validText = str => {
-    return typeof str === 'string' && str.trim().length > 0;
-  }
+	return typeof str === "string" && str.trim().length > 0;
+};
 module.exports = validText;
