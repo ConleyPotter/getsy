549bfb99fe8ccdd91fa6d75d689a152b24558788
Conley Potter <conley.potter@gmail.com>
2019-06-04 07:52:42 -0500

    Comment app.js

--- app.js
+++ app.js
@@ -8,19 +8,22 @@ const User = require('./models/User');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+// DB CONNECTION //
 mongoose 
   .connect(db, { useNewUrlParser: true})
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
+// MIDDLEWARE //
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
+// ROUTES //
 app.use("/api/users", users);
 
+// SERVER CONFIG //
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`))
