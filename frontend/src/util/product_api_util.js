ea1dbf8100b85a2cbafb4978414c40cff96b8df8
Conley Potter <conley.potter@gmail.com>
2019-06-05 08:47:53 -0500

    Add comment in product_api_util.js about backend routes, please read

--- frontend/src/util/product_api_util.js
+++ frontend/src/util/product_api_util.js
@@ -4,6 +4,9 @@ export const getProducts = () => {
 	return axios.get("/api/products/");
 };
 
+// here is something we need to discuss, do we want to have the backend route
+// be nested under users or products for grabbing all the products by a user's
+// id? -- CP
 export const getUserProducts = user_id => {
 	return axios.get(`/api/products/user/${user_id}`);
 };
