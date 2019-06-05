99c39e491030199a577495f4a0bb38a15e32ddb7
Conley Potter <conley.potter@gmail.com>
2019-06-05 17:04:48 -0500

    Additional production deployment setup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require("express");
+const path = require("path");
 
 // Route imports
 const users = require("./routes/api/users");
@@ -31,3 +32,12 @@ app.use("/api/products", products);
 // SERVER CONFIG //
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+
+// PRODUCTION CODE //
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
